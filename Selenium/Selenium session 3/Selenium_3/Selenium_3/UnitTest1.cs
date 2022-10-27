cbff953ce9638c0138e9a7688d0d95153b31b454
hoalekh <hoalekh2001@gmail.com>
2022-10-27 19:05:12 +0700

    {

---
+++ Selenium/Selenium session 3/Selenium_3/Selenium_3/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Selenium_3.Page;
+
+namespace Selenium_3
+{
+    public class Tests
+    {
+        protected WebDriverWait? _wait;
+        protected IWebDriver _driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory);
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void Test1()
+        {
+            _driver.Navigate().GoToUrl("https://www.google.com/");
+            string xpathpage = "(//div[@id='search']//a/h3)[1]";
+            string key = "Waiting for you anh yeu em hoho";
+           
+            Search googleSearch = new Search(_driver);
+            ResultPages titlePage = new ResultPages(_driver);
+            FirstResultPage resultFirstPage = new FirstResultPage(_driver,_wait);
+            googleSearch.BrowserCommandTests(key);
+            titlePage.CheckTitle(key);
+            resultFirstPage.FirstResult(xpathpage);
+
+        }
+    }
+}
